a2e2574d166fedef46a2637be4d20e27fec2df8f
Sadie Sial <sadiesial@gmail.com>
2021-09-10 01:32:58 -0500

    ward array info displaying in results box -sadie

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,20 +49,35 @@ $("#schedule-submit-button").click(function (e) {
     },
   }).done(function (data) {
     for (let i = 0; i < data.length; i++) {
+      console.log(data[i])
       wardArray.push(data[i].ward);
-      // appending data 
       // create btn ward # = value
+
+      // the user output will read as the following:
+          let makeWardOutput = 'Ward: ' + data[i].ward + ' -- Alderman: ' + data[i].alderman + ' -- Zipcode: ' + data[i].zipcode;
+
+          // ******** PSST EDGAR! This is for later if we have time, but let's leave them both displaying for now
+          let sadieTryingOutput = 'For ' + data[i].zipcode + ' the ward number is ' + data[i].ward + ' and the alderman is ' + data[i].alderman;
+
+          // builds list items in 'Results'
+          let html = `<tr><td> ${makeWardOutput} </tr></td>`;
+          let html2 = `<tr><td> ${sadieTryingOutput} </tr></td>`;
+          // empty previous results + appends new output from js line #58 (and 59)
+          $("#tablebody").empty();
+          $("#tablebody").append(html, html2);
+
     }
 
     console.log(wardArray);
   });
 
   //HERE - use wardArray to call sweeperSched
+
 });
 console.log(wardArray);
 
 
-//Calls street sweeper schedule data from API.
+//  Calls street sweeper schedule data from API.
 // var myData = undefined;
 // $.ajax(
 //   {
@@ -82,7 +97,7 @@ console.log(wardArray);
 //   })
 // );
 
-// 
+
 
 // function sweeperSched(data, currentWardNumber) {
 //   // user input times
@@ -99,7 +114,7 @@ console.log(wardArray);
 //         // console.log(data[i])
 //         if (data[i].ward === currentWardNumber) {
 //           // need to replace 'helloworld' with output
-//           let makeUserOutput =
+//           let makeWardOutput =
 //             "Ward: " +
 //             data[i].ward +
 //             " | Month: " +
@@ -107,7 +122,7 @@ console.log(wardArray);
 //             " | Date(s): " +
 //             data[i].dates;
 //           // builds list items
-//           let html = `<tr><td> ${makeUserOutput} </tr></td>`;
+//           let html = `<tr><td> ${makeWardOutput} </tr></td>`;
 //           console.log(html);
 //           // empty previous search + append the new new
 //           $("#tablebody").empty();
