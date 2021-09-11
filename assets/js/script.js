e863ce8f778bb4eb662928221731834428f8432a
Edgar Ortega <edort93@gmail.com>
2021-09-10 23:07:25 -0500

    added function to use ward number for street sweeper, moved the functions to opposite forms, added function to append information and pdf embed map

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,40 +4,12 @@ var queryURL =
   "https://data.cityofchicago.org/resource/wvjp-8m67.json" + apiKey;
 var userSearch = "";
 // var currentMonthNumber = currentDate.format("M");
+var myData = undefined;
 
-
-// .then(function (data) {
-//   // alert("Retrieved " + data.length + " records from the dataset!");
-//   console.log(data)
-// }).then(function (data))
-// //   console.log(data)
-// function searchButtonHandler(event) {
-//   event.preventDefault();
-//   let currentWardNumber = $("#zipcode").val();
-//   console.log();
-//   let zipcodeUrl = `https://data.cityofchicago.org/resource/htai-wnw4.json?zipcode=${currentWardNumber}`;
-//   console.log($("#zipcode"));
-//   $.ajax({
-//     url: zipcodeUrl,
-//     type: "GET",
-//     data: {
-//       $limit: 5000,
-//       $$app_token: "wuWBoPJo0VvB887VUDjq8qYJ8",
-//     },
-//   }).done(function (data) {
-//     console.log(data);
-//   });
-
-  // commented out until ready
-  // sweeperSched(myData, currentWardNumber);
-// }
-
-// $("#schedule-submit-button").click(searchButtonHandler());
-// var zipCodeTest = $(".zipcode-input").val();
 var wardArray = [];
-$("#schedule-submit-button").click(function (e) {
+$("#address-submit-button").click(function (e) {
   e.preventDefault();
-  var zipCodeTest = $("#zipcode").val();
+  var zipCodeTest = $("#address").val();
   console.log(zipCodeTest);
   $.ajax({
     url: `https://data.cityofchicago.org/resource/htai-wnw4.json?zipcode=${zipCodeTest}`,
@@ -49,90 +21,117 @@ $("#schedule-submit-button").click(function (e) {
     },
   }).done(function (data) {
     for (let i = 0; i < data.length; i++) {
-      console.log(data[i])
+      console.log(data[i]);
       wardArray.push(data[i].ward);
       // create btn ward # = value
 
       // the user output will read as the following:
-          let makeWardOutput = 'Ward: ' + data[i].ward + ' -- Alderman: ' + data[i].alderman + ' -- Zipcode: ' + data[i].zipcode;
+      let makeWardOutput =
+        "Ward: " +
+        data[i].ward +
+        " -- Alderman: " +
+        data[i].alderman +
+        " -- Zipcode: " +
+        data[i].zipcode;
 
-          // ******** PSST EDGAR! This is for later if we have time, but let's leave them both displaying for now, I'd like to mess with how to switch the last, first names if there's ever time to do that
-          let sadieTryingOutput = 'For ' + data[i].zipcode + ' the ward number is ' + data[i].ward + ' and the alderman is ' + data[i].alderman;
+      // ******** PSST EDGAR! This is for later if we have time, but let's leave them both displaying for now, I'd like to mess with how to switch the last, first names if there's ever time to do that
+      let sadieTryingOutput =
+        "For " +
+        data[i].zipcode +
+        " the ward number is " +
+        data[i].ward +
+        " and the alderman is " +
+        data[i].alderman;
 
-          // builds list items in 'Results'
-          let html = `<tr><td> ${makeWardOutput} </tr></td>`;
-          let html2 = `<tr><td> ${sadieTryingOutput} </tr></td>`;
-          // empty previous results + appends new output from js line #58 (and 59)
-          $("#tablebody").empty();
-          $("#tablebody").append(html, html2);
-
+      // builds list items in 'Results'
+      let html = `<tr><td> ${makeWardOutput} </tr></td>`;
+      let html2 = `<tr><td> ${sadieTryingOutput} </tr></td>`;
+      // empty previous results + appends new output from js line #58 (and 59)
+      $("#tablebody").empty();
+      $("#tablebody").append(html, html2);
     }
 
     console.log(wardArray);
   });
   //HERE - use wardArray to call sweeperSched
-
 });
 console.log(wardArray);
 
-// general btn function
-$("#address-submit-button").click(function() {
-  alert('btn works!');
+// direct ward number btn function
+$("#schedule-submit-button").click(function (e) {
+  // alert('btn works!');
+  e.preventDefault();
+  let currentWardNumber = $("#zipcode").val();
+  sweeperSched(currentWardNumber);
 });
 
+//this function will provide the appending section for the embeding of the pdf
+//added ids and styling to the frame box that has the pdf
+function getPdfHTML(ward, wardSection) {
+  return `
+  <tr><td> The ward section today being swept is ${wardSection}",you can take a look at this map to see your section 
+  <div id= "frame container"
+  style= "padding-bottom:56.25%; position:relative; display:block; width: 100%">
+  <iframe id="pdfFrame" width ="100%" height="100%" src="https://www.chicago.gov/content/dam/city/depts/streets/supp_info/2021-Street-Sweeping/Maps/sweepingWard_${ward}s.pdf" frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0"></iframe>
+  </div>`;
+}
+// code to put above to open a link in new tab instead of embed
+// <a href="https://www.chicago.gov/content/dam/city/depts/streets/supp_info/2021-Street-Sweeping/Maps/sweepingWard_${ward}s.pdf" target="_blank" >Click Here. </a></tr></td>//
 
-//  Calls street sweeper schedule data from API.
-// var myData = undefined;
-// $.ajax(
-//   {
-//     url: "https://data.cityofchicago.org/resource/wvjp-8m67.json",
-//     type: "GET",
-//     data: {
-//       $limit: 5000,
-//       $$app_token: "wuWBoPJo0VvB887VUDjq8qYJ8",
-//     },
-//     success: function (data) {
-//       sweeperSched(data);
-//     },
-//   },
-//   (success = function (data) {
-//     myData = data;
-//     console.log(data);
-//   })
-// );
+//function that takes in the ward number
+function sweeperSched(currentWardNumber) {
+  // user input times
+  let currentDate = moment();
+  let currentMonthNumber = currentDate.format("M");
+  let currentDateNumber = currentDate.format("D");
 
+  // let currentWardNumber = $("#zipcode").val();
+  console.log(currentWardNumber);
 
+  let zipcodeUrl = `https://data.cityofchicago.org/resource/wvjp-8m67.json?ward=${currentWardNumber}`;
+  // console.log($("#zipcode"));
+  // ajax call for street sweeping info
+  $.ajax({
+    url: zipcodeUrl,
+    type: "GET",
+    data: {
+      $limit: 5000,
+      $$app_token: "wuWBoPJo0VvB887VUDjq8qYJ8",
+    },
+  }).done(function (data) {
+    // console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      // $('#table' + i).text(data[i].dates);
+      if (data[i].month_number === currentMonthNumber) {
+        if (data[i].dates.split(",").includes(currentDateNumber)) {
+          // console.log(data[i])
+          if (data[i].ward === currentWardNumber) {
+           // this adds the api values to the text
+            let makeWardOutput2 =
+              "Ward: " +
+              data[i].ward +
+              " | Ward Section: " +
+              data[i].section +
+              " | Month: " +
+              data[i].month_name +
+              " | Date(s): " +
+              data[i].dates;
+              //defining more variables to use in appending the html
+            let wardSection = data[i].section;
+            let ward = data[i].ward;
 
-// function sweeperSched(data, currentWardNumber) {
-//   // user input times
-//   let currentDate = moment();
-//   let currentMonthNumber = currentDate.format("M");
-//   let currentDateNumber = currentDate.format("D");
+            // builds list items
+            let html3 = `<tr><td> ${makeWardOutput2} </tr></td>`;
 
-//   console.log(currentWardNumber);
-
-//   for (var i = 0; i < data.length; i++) {
-//     // $('#table' + i).text(data[i].dates);
-//     if (data[i].month_number === currentMonthNumber) {
-//       if (data[i].dates.split(",").includes(currentDateNumber)) {
-//         // console.log(data[i])
-//         if (data[i].ward === currentWardNumber) {
-//           // need to replace 'helloworld' with output
-//           let makeWardOutput =
-//             "Ward: " +
-//             data[i].ward +
-//             " | Month: " +
-//             data[i].month_name +
-//             " | Date(s): " +
-//             data[i].dates;
-//           // builds list items
-//           let html = `<tr><td> ${makeWardOutput} </tr></td>`;
-//           console.log(html);
-//           // empty previous search + append the new new
-//           $("#tablebody").empty();
-//           $("#tablebody").append(html);
-//         }
-//       }
-//     }
-//   }
-// }
+            console.log(html3);
+            // empty previous search + append the new new
+            $("#tablebody2").empty();
+            $("#tablebody2").append(html3);
+            //function to append the pdf url to embed in html
+            $("#tablebody2").append(getPdfHTML(ward, wardSection));
+          }
+        }
+      }
+    }
+  });
+}
